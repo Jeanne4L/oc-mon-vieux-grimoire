7f26806d46da15e17f4ac4ddd2e6c1eff322e69c
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-06-06 20:32:45 +0200

    Convert images to webp

--- controllers/book.js
+++ controllers/book.js
@@ -1,13 +1,37 @@
+const sharp = require ('sharp')
 const Book = require('../models/Book')
 
-exports.createBook = (req, res, next) => {
+exports.createBook = async (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
     delete bookObject.userId;
+
+    const { buffer, originalname } = await req.file;
+    const ref = `${originalname}-${Date.now()}.webp`;
+    // const ref = `${Date.now()}-boofdoadni.webp`
+
+    console.log(req.file)
+    // await sharp(buffer)
+    //   .webp({ quality: 80 })
+    //   .toFile("./images/" + ref);
+    // const link = `${req.protocol}://${req.get('host')}/images/${ref}`;
+
+    sharp(buffer)
+    .webp()
+    .toFile("./images/" + ref)
+    .then(function(info) {
+        console.log(info)
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
-        imageUrl: ` ${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        // imageUrl: link
+        // imageUrl: ` ${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: ` ${req.protocol}://${req.get('host')}/images/${ref}`
     });
 
     book.save()
@@ -22,6 +46,10 @@ exports.getOneBook = (req, res, next) => {
     .then(book => res.status(200).json(book))
     .catch(error => res.status(404).json({error}))
 }
+
+// exports.getBestRatings = (req, res, next) => {
+    
+// }
 
 exports.getAllBooks = (req, res, next) => {
     Book.find()
